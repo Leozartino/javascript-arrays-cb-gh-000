6c552bc14effce0ad5b7e3c44a0d80ebe8660905
Leonardo Oliveira <leozartino@gmail.com>
2019-03-27 11:53:40 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kikat",
+  "skittles"
+];
+
+function addElementToBeginningOfArray (array, element){
+  var novoArray = [...array]
+  novoArray.unshift(element)
+  return novoArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element;)
+}
+
+function addElementToEndOfArray (array, element){
+  var novoArray = [...array]
+  novoArray.push(element)
+  return novoArray;
+}
+function destructivelyAddElementToEndOfArray (array, element){
+  return array.unshift(element);
+}
